757935604cc329af81a0282681cc2e3fc8fdb2a9
sirKuybyshev <shtbklass@mail.ru>
2020-04-30 14:42:41 +0300

    The beginning

---
+++ cmake-build-debug/CMakeFiles/Coursera.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Coursera.dir/main.cpp.o"
+  "Coursera"
+  "Coursera.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Coursera.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Coursera.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Coursera.
+# This may be replaced when dependencies are built.
