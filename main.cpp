054a8bc47c46363d9ad15a56100e7ac243d644e5
timofey <shtbklass@mail.ru>
2020-07-30 16:09:16 +0300

    First part

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
+#include <algorithm>
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+  string mainPart, leftPart;
+  char ch;
+  int n;
+  cin >> mainPart;
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    leftPart += '(';
+    mainPart += ")";
+    mainPart += " ";
+    cin >> ch;
+    mainPart += ch;
+    mainPart += " ";
+    cin >> ch;
+    mainPart += ch;
+  }
+  cout << leftPart + mainPart;
+  return 0;
 }
