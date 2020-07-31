59bd8f78cb1d91bfdd5230ccd6237175218e918b
timofey <shtbklass@mail.ru>
2020-07-31 16:17:30 +0300

    Second part

--- main.cpp
+++ main.cpp
@@ -1,26 +1,32 @@
-#include <algorithm>
 #include <iostream>
 #include <string>
 
 using namespace std;
 
-
-
 int main() {
   string mainPart, leftPart;
-  char ch;
+  char type;
+  int numb;
   int n;
+  bool isNeeded = false;
   cin >> mainPart;
   cin >> n;
   for (int i = 0; i < n; i++) {
-    leftPart += '(';
-    mainPart += ")";
+    cin >> type;
+    if (((type == '*') || (type == '/')) && isNeeded) {
+      leftPart += '(';
+      mainPart += ")";
+    }
+    if ((type == '+') || (type == '-')) {
+      isNeeded = true;
+    } else {
+      isNeeded = false;
+    }
     mainPart += " ";
-    cin >> ch;
-    mainPart += ch;
+    mainPart += type;
     mainPart += " ";
-    cin >> ch;
-    mainPart += ch;
+    cin >> numb;
+    mainPart += to_string(numb);
   }
   cout << leftPart + mainPart;
   return 0;
